393c2bb2286a40d8f3db669e6279916237a6f85e
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-04-24 20:05:53 +0530

    added extra functionalities for shortcode

--- includes/short_code.php
+++ includes/short_code.php
@@ -1,5 +1,15 @@
 <?php
-function myplugin_tst_shortcode(  ){
-    return "short";
+function myplugin_tst_shortcode( $atts, $content='' ){
+    $atts = shortcode_atts( array(
+        'color' => '#0a0a0a',
+    ) , $atts); 
+    ob_start();
+    ?>
+    <div class="test">
+        <h2><?php echo $content; ?></h2>
+        <span style="color:<?php echo $atts['color'] ?>">testing</span>
+    </div>
+    <?php
+    return ob_get_clean();
 }
 add_shortcode('my-test-code' , 'myplugin_tst_shortcode');
